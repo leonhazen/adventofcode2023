81c1c5805fd742978a4eb71edba9184394a12b87
Leon Hazen <403745+leonhazen@users.noreply.github.com>
2023-12-03 18:55:38 +1100

    made a common package to hold reusable functions

---
+++ common/common.go
@@ -0,0 +1,52 @@
+package common
+
+import (
+	"strconv"
+	"unicode/utf8"
+)
+
+
+func NumToInt(num string) int {
+	// Try to convert string to int
+	// If it fails, try to convert word to int
+
+	numInt, err := strconv.Atoi(num)
+	if err == nil {
+		return numInt
+	}
+
+	switch num {
+	case "one" :
+		return 1
+	case "two":
+		return 2
+	case "three":
+		return 3
+	case "four":
+		return 4
+	case "five":
+		return 5
+	case "six":
+		return 6
+	case "seven":
+		return 7
+	case "eight":
+		return 8
+	case "nine":
+		return 9
+	default:
+		return -1
+	}
+}
+
+// stolen straight from https://stackoverflow.com/a/34521190
+func ReverseString(s string) string {
+    size := len(s)
+    buf := make([]byte, size)
+    for start := 0; start < size; {
+        r, n := utf8.DecodeRuneInString(s[start:])
+        start += n
+        utf8.EncodeRune(buf[size-start:], r)
+    }
+    return string(buf)
+}
--- day01/main.go
+++ day01/main.go
@@ -1,58 +1,13 @@
 package main
 
 import (
+	"adventofcode2024/common"
 	"bufio"
 	"fmt"
 	"os"
 	"regexp"
 	"strconv"
-	"unicode/utf8"
 )
-
-func numToInt(num string) int {
-	// Try to convert string to int
-	// If it fails, try to convert word to int
-
-	numInt, err := strconv.Atoi(num)
-	if err == nil {
-		return numInt
-	}
-
-	switch num {
-	case "one" :
-		return 1
-	case "two":
-		return 2
-	case "three":
-		return 3
-	case "four":
-		return 4
-	case "five":
-		return 5
-	case "six":
-		return 6
-	case "seven":
-		return 7
-	case "eight":
-		return 8
-	case "nine":
-		return 9
-	default:
-		return -1
-	}
-}
-
-// stolen straight from https://stackoverflow.com/a/34521190
-func reverse(s string) string {
-    size := len(s)
-    buf := make([]byte, size)
-    for start := 0; start < size; {
-        r, n := utf8.DecodeRuneInString(s[start:])
-        start += n
-        utf8.EncodeRune(buf[size-start:], r)
-    }
-    return string(buf)
-}
 
 func main() {
 	// Open the input file
@@ -78,13 +33,13 @@ func main() {
 
 		// Lets do it backwards now because apparently overlapping word numbers
 		// count as two numbers, eg "twone" would be 2 and 1
-		enil := reverse(line)
+		enil := common.ReverseString(line)
 		srebmun := rr.FindAllString(enil, -1)
 		
 		// Grab the first and last number, convert them to ints
 
-		first := numToInt(numbers[0])
-		last := numToInt(reverse(srebmun[0])) // don't forget to reverse it back
+		first := common.NumToInt(numbers[0])
+		last := common.NumToInt(common.ReverseString(srebmun[0])) // don't forget to reverse it back
 
 		// Concat them together
 
